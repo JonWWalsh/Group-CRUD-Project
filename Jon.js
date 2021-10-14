c958e0341ec691ffc2d6b4305fcc5f929f66cbc2
Jonathan Walsh <87747751+Falidian@users.noreply.github.com>
2021-10-13 23:13:17 -0600

    Initial code push, code is functional although not in the way I'd like the end product to be

--- Jon.js
+++ Jon.js
@@ -1,1 +1,12 @@
 //Updates function and GitHub host.  All-HTML layout
+const list = document.getElementById("list");
+const edit_button = document.getElementById("edit-button");
+const end_button = document.getElementById("end-editing");
+
+edit_button.addEventListener("click", function() {
+  list.contentEditable = true;
+} );
+
+end_button.addEventListener("click", function() {
+  list.contentEditable = false;
+} )
