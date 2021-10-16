fca2046462bb9b23d43a3492d3a36548af8e5e83
LexOestmann <87912594+LexOestmann@users.noreply.github.com>
2021-10-15 19:05:38 -0600

    more workable

--- Alexa.js
+++ Alexa.js
@@ -1,3 +1,4 @@
+
 // Create Recipe
 
 const recipes = [];
@@ -21,11 +22,34 @@ function renderApp() {
     const recipesDiv = document.getElementById("recipes");
     emptyElement(recipesDiv);
     for(let recipe of recipes) {
-        recipesDiv.appendChild( renderRecipe(recipe));
+        recipesDiv.appendChild(renderRecipe(recipe));
     }
 }
 
-function renderRecipe(recipe){
-    const recipeDiv = document.createElement("div")
-    mealDiv.classMeal = "mt-3";
+function renderRecipe(recipe) {
+    const recipeDiv = document.createElement("div");
+    recipeDiv.classRecipe = "mt-3";
+
+    // Ingredient Name Heading
+    const ingredientNameHeading = document.createElement("h2");
+    ingredientNameHeading.textContent = recipe.name;
+    recipeDiv.appendChild(ingredientNameHeading);
+
+    // // Ingredient Name Heading -> Delete Button
+    // const deleteButton = document.createElement("button");
+    // deleteButton.textContent = "Delete recipe";
+    // deleteButton.className = "btn btn-warning ms-2";
+    // deleteButton.addEventListener("click", () => onDeleteRecipe(recipe));
+    // ingredientNameHeading.appendChild(deleteButton);
+
+    // Ingredients Table
+    recipeDiv.appendChild(renderIngredientsTable(recipe));
+
+    // We return the <div> and it's appended on line 45
+    return recipeDiv;
+}
+
+function renderIngredientsTable(meal) {
+    const ingredientsTable = document.createElement("table");
+    ingredientsTable.className = "table table-dark table-striped";
 }
